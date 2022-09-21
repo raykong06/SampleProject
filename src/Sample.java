947e77ccdfe54311440cfbc9617fe3d697869aad
Tiffany <rkong2588@bths.edu>
2022-09-21 08:35:00 -0400

    Added favorite food to print

--- src/Sample.java
+++ src/Sample.java
@@ -2,6 +2,7 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Ray Kong");
         System.out.println("Uncut Gems");
+        System.out.println("Sushi is my favorite food");
     }
 }
 
